f719704600269fc154d5e7d0a327eeae2e858014
Julio Lobo <juliolobo@lobohost.com.br>
2023-11-17 18:14:03 -0300

    Adicionando dependências de player e monstro

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
 -- Header
 local utils = require("utils")
 
+-- Dependências
+local player = require("player.player")
+local monster = require("monster.colossus")
+
 -- Habilitar UTF-8 no terminal
 utils.enableUtf8()
 
 -- Header
 utils.printHeader()
 
+print(string.format("A vida do jogador é %d/%d", player.health, player.maxHealth))
